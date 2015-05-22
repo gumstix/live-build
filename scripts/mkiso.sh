e3c7473fb602df2560b5bcbd1911fbd42bf7a92f
Arun Bharadwaj <arun@gumstix.com>
2015-05-22 11:03:28 -0700

    Check for necessary tools installed and a few cleanups.
    Signed-off-by: Arun Bharadwaj <arun@gumstix.com>

--- scripts/mkiso.sh
+++ scripts/mkiso.sh
@@ -13,9 +13,6 @@ if [ -z "$IMAGE" ]; then
     exit 1
 fi
 
-## we must be root
-[ $(whoami) = "root" ] || { echo "E: You must be root" && exit 1; }
-
 ## Setup variables
 MLO=${PWD}/machines/${MACHINE}/MLO
 UBOOT=${PWD}/machines/${MACHINE}/u-boot.img
@@ -40,35 +37,48 @@ if [ ! -e ${ROOTFS} ]; then
     exit 1
 fi
 
+## We need these tools
+QEMUIMG=$(which qemu-img) || { echo "E: You must have qemu-img" && exit 1; }
+MKFS=$(which mkfs) || { echo "E: You must have mkfs" && exit 1; }
+KPARTX=$(which kpartx) || { echo "E: You must have kpartx" && exit 1; }
+SFDISK=$(which sfdisk) || { echo "E: You must have sfdisk" && exit 1; }
+LOSETUP=$(which losetup) || { echo "E: You must have losetup" && exit 1; }
+
+## Clean up the loop devices
+sudo losetup -D
+
+## Prepare mount point
+sudo umount -f /mnt &>/dev/null
+
 echo "I: Creating a raw image file"
 IMGFILE=${MACHINE}-${IMAGE}.img
-qemu-img create -f raw ${IMGFILE} 4G
+${QEMUIMG} create -f raw ${IMGFILE} 4G
 
 echo "I: Partition the device"
 {
 echo 128,130944,0x0C,*
 echo 131072,,,-
-} | sudo /sbin/sfdisk -D -uS -H 255 -S 63 ${IMGFILE} --force
+} | sudo ${SFDISK} -D -uS -H 255 -S 63 ${IMGFILE} --force
 
-LOOPDEV=` sudo kpartx -av ${IMGFILE} | sed -n 1p | awk '{print $3}' | cut -c 1-5`
+LOOPDEV=` sudo ${KPARTX} -av ${IMGFILE} | sed -n 1p | awk '{print $3}' | cut -c 1-5`
 LOOPDEV_BOOT=/dev/mapper/${LOOPDEV}p1
 LOOPDEV_ROOT=/dev/mapper/${LOOPDEV}p2
 
 echo "I: Set up the boot partition"
-sudo mkfs.vfat -F 32 -n boot ${LOOPDEV_BOOT}
+sudo ${MKFS}.vfat -F 32 -n boot ${LOOPDEV_BOOT}
 sudo mount ${LOOPDEV_BOOT} /mnt
-cp -v ${MLO} ${UBOOT} ${UENV} /mnt/
+sudo cp -v ${MLO} ${UBOOT} ${UENV} /mnt/
 sync
 sudo umount /mnt
 
 echo "I: Set up the rootfs partition"
-sudo mkfs.ext4 -L rootfs ${LOOPDEV_ROOT}
+sudo ${MKFS}.ext4 -L rootfs ${LOOPDEV_ROOT}
 sudo mount ${LOOPDEV_ROOT} /mnt
 sudo tar xaf ${ROOTFS} --strip-components=1 -C /mnt
 sync
 sudo umount /mnt
 
-sudo kpartx -dv ${IMGFILE}
+sudo ${KPARTX} -dv ${IMGFILE}
 
 echo "I: Creating a compressed image"
 gzip -9 ${IMGFILE}
